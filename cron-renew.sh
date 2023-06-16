83df38aaea0cdc024cd6b919e4c665b2074ff619
root <root@prqc1>
2023-06-16 19:02:25 +0200

    HOTFIX: acme.sh seems to append _ecc to the dir names?

--- cron-renew.sh
+++ cron-renew.sh
@@ -10,8 +10,8 @@ source .env
 /root/.acme.sh/acme.sh --cron --home "/root/.acme.sh"
 
 # Copy to place
-install -o caddy -g caddy -m 644 "/root/.acme.sh/$DOMAIN/fullchain.cer" /etc/caddy/vmtree.crt
-install -o caddy -g caddy -m 600 "/root/.acme.sh/$DOMAIN/$DOMAIN.key" /etc/caddy/vmtree.key
+install -o caddy -g caddy -m 644 /root/.acme.sh/"$DOMAIN"*/fullchain.cer /etc/caddy/vmtree.crt
+install -o caddy -g caddy -m 600 /root/.acme.sh/"$DOMAIN"*/"$DOMAIN".key /etc/caddy/vmtree.key
 
 # Restart Caddy
 systemctl restart caddy.service
